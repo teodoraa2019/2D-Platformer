0d7401cc021235887145a342f139304b6de509e6
Teodora Vodopija <48692987+teodoraa2019@users.noreply.github.com>
2020-04-14 23:10:02 +0200

    ..

---
+++ 2D Platformer/Assets/Skripte/PadPlatforme.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadPlatforme : MonoBehaviour {
+
+    public float fallDelay = 1f;
+    private Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke ("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
